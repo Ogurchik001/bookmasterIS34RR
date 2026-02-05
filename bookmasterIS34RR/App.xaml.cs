831eaea8186200a64f3fb0201465718e6042156a
vboxuser <vboxuser@WIN10>
2026-02-05 16:33:33 +0300

    Добавьте файлы проекта.

---
+++ bookmasterIS34RR/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace bookmasterIS34RR
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        
+    }
+
+}
