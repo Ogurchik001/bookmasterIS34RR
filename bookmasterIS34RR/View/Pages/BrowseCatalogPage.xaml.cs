9f51bb5a1953a5df1d2f7bb794eb4def6601960b
vboxuser <vboxuser@WIN10>
2026-02-20 10:33:59 +0300

    Изменены файлы проекта

--- bookmasterIS34RR/View/Pages/BrowseCatalogPage.xaml.cs
+++ bookmasterIS34RR/View/Pages/BrowseCatalogPage.xaml.cs
@@ -24,5 +24,10 @@ namespace bookmasterIS34RR.View.Pages
         {
             InitializeComponent();
         }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
